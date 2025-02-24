3385a40e994d15ed7fa561a7c16859a07c4f68e3
Jatin-Madhikarmi <jatinmadhikarmi8@gmail.com>
2025-02-24 15:57:11 +0545

    The traffic light was partially completed i.e the vehicles keep toggling between 0 and 1 in the interval of 10 seconds everyime now to create a proper traffic light system.

--- src/Dummy.cpp
+++ src/Dummy.cpp
@@ -7,65 +7,72 @@ vehicle::vehicle():
 x(675),
 y(0),
 size(50),
-speed(2)
+speed(1)
 {
 }
 
-void vehicle:: update() {
+void vehicle::update() {
     const int screenHeight = GetScreenHeight();
-        int state = 0;
-        std::ifstream file("VehiclesNo.txt");
-        if (file.is_open()) {
-            file >> state;
-            file.close();
+    static float lastUpdateTime = GetTime();  // Static variable to store the last update time
+    int state = 0;
+
+    // Read the number of vehicles
+    std::ifstream file("VehiclesNo.txt");
+    if (file.is_open()) {
+        file >> state;
+        file.close();
+    } else {
+        TraceLog(LOG_WARNING, "Could not open traffic light file for reading!");
+        return;
+    }
+    vehicleActiveStates.resize(state, true);
+
+    // Read the current traffic state
+    std::ifstream trafficFile("D&CTrafficLight.txt");
+    if (trafficFile.is_open()) {
+        trafficFile >> trafficState;
+        trafficFile.close();
+    } else {
+        TraceLog(LOG_WARNING, "Couldn't able to open D&CTrafficLight.txt");
+        return;
+    }
+
+    // Check if 10 seconds have passed since the last update
+    float currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= 10) {
+        // Toggle the traffic state
+        trafficState = (trafficState == 0) ? 1 : 0;
+
+        // Update the traffic state in the file
+        std::ofstream trafficFile("D&CTrafficLight.txt");
+        if (trafficFile.is_open()) {
+            trafficFile << trafficState;
+            trafficFile.close();
         } else {
-            TraceLog(LOG_WARNING, "Could not open traffic light file for reading!");
-            return;
+            printf("Failed to open D&CTrafficLight.txt for writing.\n");
         }
-        vehicleActiveStates.resize(state,true);
 
-        std::ifstream File("D&CTrafficLight.txt");
-        if(File.is_open())
-        {
-            printf("The file D&CTrafficLight is open and read properly.\n");
-            File>>trafficState;
-            printf("trafficState from the file is : %d\n",trafficState);
-            File.close();
-        }
-        else
-        {
-            TraceLog(LOG_WARNING,"Couldn't able to open D&CTrafficLight.txt");
-            return;
-        }
-        printf("trafficState: %d\n", trafficState);
-        if (trafficState==1)
-        {
-            for(int i=0;i<state;i++)
-            {
-                if(vehicleActiveStates[i]==true)
-                {
-                    vehicleYPositions[i]+=speed;
-                    if(vehicleYPositions[i]+size>=screenHeight)
-                    {
-                        printf("Collision detected for the vehicle %d\n",i);
-                        vehicleActiveStates[i]=false;
-                    }
+        // Update the last update time
+        lastUpdateTime = currentTime;
+    }
+
+    // If traffic state is 1, move the vehicles
+    if (trafficState == 1) {
+        for (int i = 0; i < state; i++) {
+            if (vehicleActiveStates[i] == true) {
+                vehicleYPositions[i] += speed;
+                if (vehicleYPositions[i] + size >= screenHeight) {
+                    printf("Collision detected for the vehicle %d\n", i);
+                    vehicleActiveStates[i] = false;
                 }
-    
             }
-
         }
-        else
-        {
-            int timeelapsed=GetTime();
-            if()
-        }
+    }
+}
 //here the update function collision's only detected when the the last vehicle goes out of the scope then the whole collison is detected 
 //and the vehicle are no longer rendered onto the screen
-
-}
-
-void vehicle::draw() {
+    
+    void vehicle::draw() {
     int state = 0;
         std::ifstream file("VehiclesNo.txt");
         if (file.is_open()) 
